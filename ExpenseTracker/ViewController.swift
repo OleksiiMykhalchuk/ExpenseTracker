05f7ddcd14806bd021e2dee845cf797f29be930e
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-04-30 14:41:30 +0200

    install SwiftLint

--- ExpenseTracker/ViewController.swift
+++ ExpenseTracker/ViewController.swift
@@ -7,17 +7,9 @@
 
 import UIKit
 
-class testCommit {
-    
-}
-
 class ViewController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
