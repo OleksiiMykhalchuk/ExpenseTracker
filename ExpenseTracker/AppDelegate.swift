b7571bfb39bac24f7e398e126dc32cd302eb43a3
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-04-30 17:57:10 +0200

    Add variable applicationDocumentDirectory

--- ExpenseTracker/AppDelegate.swift
+++ ExpenseTracker/AppDelegate.swift
@@ -14,7 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        print(applicationDocumentsDirectory)
+        
         return true
     }
 
@@ -59,6 +61,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         })
         return container
+    }()
+    
+    let applicationDocumentsDirectory: URL = {
+        let paths = FileManager.default.urls(
+            for: .documentDirectory,
+            in: .userDomainMask)
+        return paths[0]
     }()
 
     // MARK: - Core Data Saving support
