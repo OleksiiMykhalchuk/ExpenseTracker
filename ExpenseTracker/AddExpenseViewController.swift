0a128970630633fb5ff64676e71821c7c50ad1a9
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-05-02 17:42:04 +0200

    Add Icons, Add AddExpenseViewController class

---
+++ ExpenseTracker/AddExpenseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseViewController.swift
+//  ExpenseTracker
+//
+//  Created by Oleksii Mykhalchuk on 5/2/22.
+//
+
+import UIKit
+
+class AddExpenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+}
