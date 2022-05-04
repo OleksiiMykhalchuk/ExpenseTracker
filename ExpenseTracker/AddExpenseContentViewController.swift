4886a19d26eed47105c4a48d06535b87ccbc0645
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-05-04 19:21:08 +0200

    Add Save Button, global tint AccentColor in scene

--- ExpenseTracker/AddExpenseContentViewController.swift
+++ ExpenseTracker/AddExpenseContentViewController.swift
@@ -11,6 +11,20 @@ class AddExpenseContentViewController: UITableViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var addButton: UIButton!
+    @IBAction func buttonPressed() {
+        textField.text = ""
+        let alert = UIAlertController(
+            title: "Saved",
+            message: "Success!!!",
+            preferredStyle: .alert)
+        let action = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: {self.buttonPressed()})
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -22,19 +36,16 @@ class AddExpenseContentViewController: UITableViewController {
         return 20
     }
     override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 20
+        return 1
     }
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
     }
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    override func tableView(
+        _ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.layer.masksToBounds = false
         cell.layer.borderWidth = 1
         cell.layer.cornerRadius = 8
         cell.layer.borderColor = UIColor(named: "Green2")?.cgColor
-    }
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        self.tableView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
 }
