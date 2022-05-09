b550b493c7c507074d95ba5473f2e2def7d5a2c5
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-05-09 12:36:36 +0200

    reorganize storyboards and files to the folders

---
+++ ExpenseTracker/Category/CategoryDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CategoryDetailViewController.swift
+//  ExpenseTracker
+//
+//  Created by Oleksii Mykhalchuk on 5/8/22.
+//
+
+import UIKit
+
+class CategoryDetailViewController: UITableViewController {
+    // MARK: - Outlets
+  @IBOutlet weak var textField: UITextField!
+  @IBOutlet weak var doneButtonItem: UIBarButtonItem!
+  // MARK: - Variables
+  var categoryName: String?
+  var barButton = UIBarButtonItem()
+  // MARK: - Actions
+  @IBAction func doneButton() {
+    dismiss(animated: true)
+  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      textField.becomeFirstResponder()
+      title = "Add Category"
+      textFieldConfiguration()
+  }
+  override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+    guard let header = view as? UITableViewHeaderFooterView else { return }
+    header.textLabel!.font = UIFont(name: "Inter", size: 12)
+  }
+  override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    return 20
+  }
+  // MARK: - Helper Methods
+  private func textFieldConfiguration() {
+    textField.text = categoryName
+    navigationItem.setRightBarButton(barButton, animated: true)
+    textField.layer.borderWidth = 1
+    textField.layer.borderColor = UIColor(named: "Green2")?.cgColor
+    textField.layer.cornerRadius = 8
+    textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+    textField.leftViewMode = .always
+  }
+}
