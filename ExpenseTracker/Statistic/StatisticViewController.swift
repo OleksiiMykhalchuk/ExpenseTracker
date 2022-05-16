f6459ffb7202ae5e4a49c980df5fc777331cb620
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-05-16 15:53:46 +0200

    Graph View

---
+++ ExpenseTracker/Statistic/StatisticViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryViewController.swift
+//  ExpenseTracker
+//
+//  Created by Oleksii Mykhalchuk on 5/16/22.
+//
+
+import UIKit
+
+class StatisticViewController: UIViewController {
+  @IBOutlet weak var viewGraph: UIView!
+  let label = UILabel()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      title = "Statistic"
+      viewGraph.layer.borderWidth = 1
+      label.text = "Graphs View"
+      label.bounds.size = CGSize(width: 100, height: 20)
+      label.center = viewGraph.center
+      label.layer.borderWidth = 1
+      viewGraph.addSubview(label)
+    }
+}
