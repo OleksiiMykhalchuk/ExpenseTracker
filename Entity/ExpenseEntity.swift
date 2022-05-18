b7b93a5f12df25560ee2db742d71d87b90117c27
Denysov Illia <illia.denysov@helplightning.com>
2022-05-18 21:14:53 +0300

    Added simple models

---
+++ Entity/ExpenseEntity.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseEntity.swift
+//  ExpenseTracker
+//
+//  Created by Denysov Illia on 18.05.2022.
+//
+
+import Foundation
+
+struct ExpenseEntity {
+    let date: Date
+    let amount: Double
+    let category: String
+}
